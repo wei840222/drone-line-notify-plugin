a28883a7ea577aa6a03146d6468bdb88df6bb1e7
wei840222 <wei840222@gmail.com>
2019-07-11 23:14:19 +0800

    add muti token

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
-(async function () {
-    const axios = require('axios')
-    
-    console.log(process.env)
+const axios = require('axios')
 
-    const {
-        DRONE_REPO_NAME,
-        DRONE_COMMIT_MESSAGE,
-        DRONE_COMMIT_BRANCH,
-        DRONE_BUILD_EVENT,
-        DRONE_COMMIT_AUTHOR,
-        CI_BUILD_NUMBER,
-        DRONE_BUILD_STATUS,
-        DRONE_COMMIT_LINK,
-        PLUGIN_ACCESS_TOKEN,
-        DRONE_BUILD_NUMBER
-    } = process.env
+const {
+    DRONE_REPO_NAME,
+    DRONE_COMMIT_MESSAGE,
+    DRONE_COMMIT_BRANCH,
+    DRONE_BUILD_EVENT,
+    DRONE_COMMIT_AUTHOR,
+    CI_BUILD_NUMBER,
+    DRONE_BUILD_STATUS,
+    DRONE_COMMIT_LINK,
+    DRONE_BUILD_LINK,
+    PLUGIN_ACCESS_TOKEN,
+    DRONE_BUILD_NUMBER
+} = process.env
 
+PLUGIN_ACCESS_TOKEN.split(',').array.forEach(async token => {
     await axios.post('https://notify-api.line.me/api/notify',
         `message=
 Repo: ${DRONE_REPO_NAME}
@@ -25,13 +24,14 @@ Event: ${DRONE_BUILD_EVENT}
 Commit Message: ${DRONE_COMMIT_MESSAGE}
 Drone Build number: ${CI_BUILD_NUMBER || DRONE_BUILD_NUMBER}
 Drone Build status: ${DRONE_BUILD_STATUS}
+Build: ${DRONE_BUILD_LINK}
 Changes: ${DRONE_COMMIT_LINK}`,
         {
             headers:
             {
                 'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': `Bearer ${PLUGIN_ACCESS_TOKEN}`
+                'Authorization': `Bearer ${token}`
             }
         }
     ).catch(err => console.log(err))
-})()
+});
